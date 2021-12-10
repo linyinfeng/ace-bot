45264344ffe13829bc312d10ecbb26bc3a6745ec
Lin Yinfeng <lin.yinfeng@outlook.com>
2021-12-10 16:19:38 +0800

    Add nixConfig

--- flake.nix
+++ flake.nix
@@ -1,4 +1,8 @@
 {
+  nixConfig.extra-experimental-features = "nix-command flakes ca-references";
+  nixConfig.extra-substituters = "https://linyinfeng.cachix.org https://nix-community.cachix.org";
+  nixConfig.extra-trusted-public-keys = "linyinfeng.cachix.org-1:sPYQXcNrnCf7Vr7T0YmjXz5dMZ7aOKG3EqLja0xr9MM= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils-plus.url = "github:gytis-ivaskevicius/flake-utils-plus";
